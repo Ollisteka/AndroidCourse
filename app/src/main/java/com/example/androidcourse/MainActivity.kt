cce2a49acd1a0306d60be1d21634d9c4a73f39b2
Olga Zhukova <zhukova.o.m@yandex.ru>
2020-02-28 08:26:29 +0500

    Вывожу на экран первого активити чиселку, увеличиваю её на 1 при изменении конфигурации

--- app/src/main/java/com/example/androidcourse/MainActivity.kt
+++ app/src/main/java/com/example/androidcourse/MainActivity.kt
@@ -4,15 +4,30 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
+const val COUNTER = "COUNTER"
+
 class MainActivity : AppCompatActivity() {
-    var counter: Int = 0;
+    private var counter: Int = 0;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        counter++
+        printCounter()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(COUNTER, counter + 1)
+
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        counter = savedInstanceState.getInt(COUNTER, 0)
+        printCounter()
+    }
+
+    private fun printCounter() {
         val textView = findViewById<TextView>(R.id.counter)
         textView.text = "$counter"
     }
-
 }
