0536e7195163d3c2d77ec010e667f834978b8bdf
Olga Zhukova <zhukova.o.m@yandex.ru>
2020-03-07 13:06:15 +0500

    Сверстала кнопочку для добавления привычки

--- app/src/main/java/com/example/androidcourse/MainActivity.kt
+++ app/src/main/java/com/example/androidcourse/MainActivity.kt
@@ -2,11 +2,11 @@ package com.example.androidcourse
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-const val COUNTER = "COUNTER"
 const val EXTRA_COUNTER = "com.example.androidcourse.COUNTER"
 const val LOG_TAG = "MainActivity"
 
@@ -15,19 +15,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
 
-    private val habits: List<Habit> = mutableListOf(
-        Habit("Привычка", "Описание", 1, 10, "white"),
-        Habit("Привычка", "Описание", 1, 10, "white"),
-        Habit("Привычка", "Описание", 1, 10, "white"),
-        Habit("Привычка", "Описание", 1, 10, "white"),
-        Habit("Привычка", "Описание", 1, 10, "white"),
-        Habit("Привычка", "Описание", 1, 10, "white"),
-        Habit("Привычка", "Описание", 1, 10, "white"),
-        Habit("Привычка", "Описание", 1, 10, "white"),
-        Habit("Привычка", "Описание", 1, 10, "white"),
-        Habit("Привычка", "Описание", 1, 10, "white"),
-        Habit("Привычка", "Описание", 1, 10, "white"),
-        Habit("Привычка", "Описание", 1, 10, "white")
+    private val habits: MutableList<Habit> = mutableListOf(
+        Habit("Привычка #1", "Описание", 1, 10, "white")
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,6 +37,12 @@ class MainActivity : AppCompatActivity() {
             // specify an viewAdapter (see also next example)
             adapter = viewAdapter
 
+        }
+
+        val addHabitButton: View = findViewById(R.id.addHabitButton)
+        addHabitButton.setOnClickListener { view ->
+            habits.add(Habit("Привычка #${habits.size + 1}", "Описание", 1, 10, "white"))
+            viewAdapter.notifyItemInserted(habits.size - 1)
         }
     }
 
